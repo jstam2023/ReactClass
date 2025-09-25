8dfbb5dd80d56a2ef9cd46226fc41c54a1c596b6
jstam2023 <jstam2023@gmail.com>
2025-09-25 21:08:47 +0300

    feat: added FunctionalComponentWithState with CounterButton components and Props

--- src/components/FunctionalComponentWithState.tsx
+++ src/components/FunctionalComponentWithState.tsx
@@ -1,20 +1,27 @@
 import {useState} from "react";
+import CounterButton from "./CounterButton.tsx";
 const FunctionalComponentWithState = ()=> {
 
     const [count,setCount] = useState(0)
+    const [disabled, setDisabled] = useState(true);
+
 
     const increaseCount = () => {
         setCount(count+1)
+        setDisabled(false)
     }
 
     const decreaseCount = () => {
-        if(count>0){
+        if(count === 1 ) {
+            setDisabled(true)
+        } if(count>=1){
             setCount(count-1)
         }
     }
 
     const reset = () => {
         setCount(0)
+        setDisabled(true)
     }
 
 
@@ -24,15 +31,10 @@ const FunctionalComponentWithState = ()=> {
             <div className="space-y-4 pt-12">
                 <h1 className="text-center py-2 font-bold text-xl">Count is {count}</h1>
                 <div className="text-center">
-                    <button onClick={increaseCount}
-                            className="hover:text-green-500 bg-black text-white rounded-xl py-2 px-4 mx-2">Increase
-                    </button>
-                    <button onClick={decreaseCount}
-                            className="hover:text-red-500 bg-black text-white rounded-xl py-2 px-4 mx-2">Decrease
-                    </button>
-                    <button onClick={reset}
-                            className="hover:text-blue-500 bg-black text-white rounded-xl py-2 px-4 mx-2">Reset
-                    </button>
+                    <CounterButton onClick={increaseCount} disabled={false} label="Increase"/>
+                    <CounterButton onClick={decreaseCount} disabled={disabled} label="Decrease" addClass="bg-cf-dark-red"/>
+                    <CounterButton onClick={reset} disabled={disabled} label="Reset" addClass="bg-green-400 hover:text-black"/>
+
                 </div>
             </div>
 
