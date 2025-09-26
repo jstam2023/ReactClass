05b9fb99d194ecd663bb215836a549bbc5b875c1
jstam2023 <jstam2023@gmail.com>
2025-09-26 21:56:14 +0300

    feat: added CounterWithReducer component

---
+++ src/components/CounterWithReducer.tsx
@@ -0,0 +1,31 @@
+import CounterButton from "./CounterButton.tsx";
+import {useCounterWithReducer} from "../hooks/useCounterWithReducer.ts";
+// import {useCounter} from "../hooks/useCounter.ts";
+
+const CounterWithReducer = () => {
+
+    const {count, lastAction, time, increase, decrease, reset} = useCounterWithReducer()
+
+
+
+    return (
+        <>
+            <div className="space-y-4 pt-10">
+                <h1 className="text-center font-bold text-xl">Count is {count}</h1>
+
+                <div className="text-center py-4">
+                    <CounterButton onClick={increase} disabled={false} label="Increase"/>
+                    <CounterButton onClick={decrease} disabled={count === 0} label="Decrease" addClass="bg-cf-dark-red"/>
+                    <CounterButton onClick={reset} disabled={count === 0} label="Reset" addClass="bg-green-400 hover:text-black"/>
+                </div>
+                <div className="text-center">
+                    <span className="mx-4">Last change: <span className="font-bold">{lastAction || "-"}</span></span>
+                    <span> at: <span className="font-bold">{time || "-"}</span></span>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default CounterWithReducer;
