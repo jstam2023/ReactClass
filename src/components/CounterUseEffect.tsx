1986b219cfa383768b1f3b770b1f2cc6d4777302
jstam2023 <jstam2023@gmail.com>
2025-09-28 22:51:57 +0300

    feat: add CounterUseEffect component with switching button name and hover color

---
+++ src/components/CounterUseEffect.tsx
@@ -0,0 +1,46 @@
+import {useEffect, useState} from "react";
+
+const CounterUseEffect = () => {
+    const [secs, setSecs] = useState(0);
+    const [stop, setStop] = useState(true);
+    const test = stop ? ` hover:text-green-400` : ` hover:text-red-400`
+
+
+    useEffect(() => {
+        document.title = "CounterUseEffect | JStam";
+    },[])
+
+
+
+    const timer = setInterval(()=>{
+        if(!stop){
+            setSecs(secs+1);
+        }
+        else if(stop){
+            clearInterval(timer);
+        }
+    },1000)
+
+
+
+
+
+
+
+    return (
+        <>
+            <div className="text-center">
+                <h1 className="text-center py-2 text-xl">Seconds: {secs}</h1>
+                <button
+                    onClick={() => setStop(!stop)}
+
+                    className={"rounded bg-cf-dark-gray text-white w-14" + test }>{stop ? "Start" : "Stop"}</button>
+                <h3 className="py-4">Total Seconds: {secs}</h3>
+            </div>
+
+
+        </>
+    )
+}
+
+export default CounterUseEffect;
