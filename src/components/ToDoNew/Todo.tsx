c05c7ff72b569a7552d7c65198c641b7dbe5a47c
jstam2023 <jstam2023@gmail.com>
2025-10-09 20:35:40 +0300

    feat: install React Router

--- src/components/ToDoNew/Todo.tsx
+++ src/components/ToDoNew/Todo.tsx
@@ -30,7 +30,9 @@ const todoReducer = (state: TodoProps[], action:Action): TodoProps[] => {
             todo.id === action.payload
                 ? {...todo, completed: !todo.completed}
                 : todo
-            )
+            );
+        case "CLEAR_ALL":
+            return [];
         default:
             return state;
     }
@@ -38,6 +40,9 @@ const todoReducer = (state: TodoProps[], action:Action): TodoProps[] => {
 
 const Todo = () => {
     const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos);
+    const totalTasks: number = todos.length
+    const completedTasks: number = todos.filter(t=>t.completed).length;
+    const activeTasks: number = totalTasks - completedTasks
 
     useEffect(()=> {
         localStorage.setItem("todos", JSON.stringify(todos));
@@ -46,6 +51,11 @@ const Todo = () => {
     useEffect(() => {
         document.title = "ToDoApp | JStam"
     }, []);
+
+    const handleClearAll = ()=>{
+        dispatch({type: "CLEAR_ALL"})
+    }
+
     return (
         <>
             <div className="max-w-sm mx-auto p-6">
@@ -54,9 +64,26 @@ const Todo = () => {
                 <TodoList todos={todos} dispatch={dispatch}/>
 
 
+
+                {todos.length > 0 && (
+                    <>
+                        <div className="flex justify-between border-t pt-2 mt-4 text-cf-gray">
+                            <span>Total: {totalTasks}</span>
+                            <span>Active: {activeTasks}</span>
+                            <span>Completed: {completedTasks}</span>
+                        </div>
+                        <div className="text-end mt-4">
+                            <button
+                                onClick={handleClearAll}
+                                className="bg-cf-dark-red text-white py-2 px-4 rounded">Clear All</button>
+
+                        </div>
+                    </>
+                )}
+
+
+
             </div>
-
-
         </>
     )
 }
--- src/components/ToDoNew/types.ts
+++ src/components/ToDoNew/types.ts
@@ -10,6 +10,7 @@ export type Action =
     | {type: "DELETE"; payload: number}
     | {type: "EDIT"; payload: {id: number; newText:string}}
     | {type: "COMPLETE"; payload: number}
+    | {type: "CLEAR_ALL"; payload: number}
 
 export type TodoFormProps = {
     dispatch: React.Dispatch<Action>
